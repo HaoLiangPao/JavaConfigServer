e53e6f43210682c42048179eb66459545a570a80
Hao Liang (LCL) <Hao.Liang@loblaw.ca>
2021-11-03 22:59:45 -0400

    Config Server Set up Failed, seems that there is no way to buld the app, parent version seems to be incorrect :(

--- spring-cloud-config-server/src/main/java/javaLearning/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/javaLearning/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
