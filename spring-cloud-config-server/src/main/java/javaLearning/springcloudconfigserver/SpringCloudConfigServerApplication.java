f705636d445c72da584077e6cc1b49df74474dba
Hao Liang (LCL) <Hao.Liang@loblaw.ca>
2021-11-04 22:47:07 -0400

    Config Server Template Successfully Built

--- spring-cloud-config-server/src/main/java/javaLearning/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/javaLearning/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableConfigServer
 public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
