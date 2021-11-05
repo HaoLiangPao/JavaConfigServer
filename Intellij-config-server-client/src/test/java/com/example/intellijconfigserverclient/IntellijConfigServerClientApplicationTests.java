4bb0c194107e27692133aa8d4fe1ca8a5219f4b1
Hao Liang (LCL) <Hao.Liang@loblaw.ca>
2021-11-04 23:11:40 -0400

    Client Server successfully build

--- Intellij-config-server-client/src/test/java/com/example/intellijconfigserverclient/IntellijConfigServerClientApplicationTests.java
+++ Intellij-config-server-client/src/test/java/com/example/intellijconfigserverclient/IntellijConfigServerClientApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.example.intellijconfigserverclient;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class IntellijConfigServerClientApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package com.example.intellijconfigserverclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellijConfigServerClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
