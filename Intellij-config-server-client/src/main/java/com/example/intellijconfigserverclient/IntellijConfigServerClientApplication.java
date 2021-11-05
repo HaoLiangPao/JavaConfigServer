eb456a2b358bf1cf0432fa861ef5d19bf54cc8b5
Hao Liang (LCL) <Hao.Liang@loblaw.ca>
2021-11-04 22:48:08 -0400

    Config Server Simple Web Client Set Up

---
+++ Intellij-config-server-client/src/main/java/com/example/intellijconfigserverclient/IntellijConfigServerClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.intellijconfigserverclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellijConfigServerClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntellijConfigServerClientApplication.class, args);
+    }
+
+}
---
+++ Intellij-config-server-client/src/test/java/com/example/intellijconfigserverclient/IntellijConfigServerClientApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.example.intellijconfigserverclient;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class IntellijConfigServerClientApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
